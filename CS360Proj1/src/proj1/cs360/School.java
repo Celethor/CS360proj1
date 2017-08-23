1274e179042674c0b65fa4a2033088fb3f4b1ef6
Ben <Ben@serebii.ad.ipfw.edu>
2017-08-23 15:42:51 -0400

    merged changes

--- CS360Proj1/src/proj1/cs360/School.java
+++ CS360Proj1/src/proj1/cs360/School.java
@@ -1,10 +1,12 @@
 package proj1.cs360;
+
 /*
  * This class contains all of the fields and methods necessary to
  * 
  * 
  * 
  */
+
 public class School {
 
 	private String name;
@@ -13,6 +15,32 @@ public class School {
 	private boolean willingHost;
 	private char classification;
 	
+	public School(){
+		name=new String("");
+		location=new String("");
+		enrollment=0;
+		willingHost=false;
+		classification='N';
+	}
+	public School(String name,String location, int enrollment, boolean willingHost){
+		this.name=name;
+		this.location=location;
+		this.enrollment=enrollment;
+		this.willingHost=willingHost;
+		this.classification=classify(this.enrollment);
+	}
+	private char classify(int enrollment){
+		char classification;
+		if(enrollment>1800)
+			classification='A';
+		else if(enrollment>1000)
+			classification='B';
+		else if(enrollment>500)
+			classification='C';
+		else 
+			classification='D';
+		return classification;
+	}
 	
 	private String lookupAddr(){
 	return "";
