587bb3a9a18294ef66f3df2ae083232773a834f6
Dell <Dell@DESKTOP-OLINNH0.ad.ipfw.edu>
2017-08-23 15:30:29 -0400

    Added constructor to school class

--- CS360Proj1/src/proj1/cs360/School.java
+++ CS360Proj1/src/proj1/cs360/School.java
@@ -8,5 +8,18 @@ public class School {
 	private boolean willingHost;
 	private char classification;
 	
-	
+	public School(){
+		name=new String("");
+		location=new String("");
+		enrollment=0;
+		willingHost=false;
+		classification='N';
+	}
+	public School(String name,String location, int enrollment, boolean willingHost, char classification){
+		this.name=name;
+		this.location=location;
+		this.enrollment=enrollment;
+		this.willingHost=willingHost;
+		this.classification=classification;
+	}
 }
