8e1089885cb44675d1f018d387ed9e51cec96310
Benjamin <Benjamin@SapphireFervor>
2017-08-25 03:30:31 -0400

    -Generated getters for School Class -fixed Constructor in school claass to utilize new address lookup function

--- CS360Proj1/src/proj1/cs360/School.java
+++ CS360Proj1/src/proj1/cs360/School.java
@@ -1,7 +1,9 @@
 package proj1.cs360;
 
+
 /*
  * This class contains all of the fields and methods necessary to
+ * create a wonderful school object
  * 
  * 
  * 
@@ -22,13 +24,31 @@ public class School {
 		willingHost=false;
 		classification='N';
 	}
-	public School(String name,String location, int enrollment, boolean willingHost){
+	public School(String name, int enrollment, boolean willingHost){
 		this.name=name;
-		this.location=location;
+		this.location= lookupAddr();		
 		this.enrollment=enrollment;
 		this.willingHost=willingHost;
 		this.classification=classify(this.enrollment);
 	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public int getEnrollment() {
+		return enrollment;
+	}
+	public boolean isWillingHost() {
+		return willingHost;
+	}
+	public char getClassification() {
+		return classification;
+	}
+	
+	//
 	private char classify(int enrollment){
 		char classification;
 		if(enrollment>1600)
@@ -40,7 +60,13 @@ public class School {
 		return classification;
 	}
 	
+	//used in contructor, gets address by looking up name via google maps
 	private String lookupAddr(){
-	return "";
+		try {
+			return EarthSearch.lookupAddress(this.name + ", Indiana");
+		} catch (Exception e) {
+			return "Error locating School";
+		}
 	}
+	
 }
