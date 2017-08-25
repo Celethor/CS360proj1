d4dc35392b838e5c03269b37e92d36d20b7b21bb
Benjamin <Benjamin@SapphireFervor>
2017-08-25 02:54:02 -0400

    Finished EarthSearch Class by adding method to calculate drive distance between two locations give both addresses

--- CS360Proj1/src/proj1/cs360/BensTestingClass.java
+++ CS360Proj1/src/proj1/cs360/BensTestingClass.java
@@ -8,9 +8,12 @@ public class BensTestingClass {
 
 	public static void main(String[] args) throws ApiException, InterruptedException, IOException {
 		
-		String establishment = "Snider High School, Indiana";
-		EarthSearch.lookupAddress(establishment);
+		//String establishment = "Snider High School, Indiana";
+		//EarthSearch.lookupAddress(establishment);
 
+	//	System.out.println(EarthSearch.computeDriveDistance("5901 Heywood Cove, Fort Wayne, IN 46815",
+	//			"4600 Fairlawn Pass, Fort Wayne, IN 46815"));
+		
 	}
 
 }
--- CS360Proj1/src/proj1/cs360/EarthSearch.java
+++ CS360Proj1/src/proj1/cs360/EarthSearch.java
@@ -1,35 +1,71 @@
 package proj1.cs360;
 
-/*
- * THIs Class contains methods to lookup the address of an establishment given its name, 
- * It also contains a method to determin the distance betweeen 2 establishments
- * 
- */
-
 import java.io.IOException;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.maps.GaeRequestHandler;
+import com.google.maps.DirectionsApi.RouteRestriction;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.errors.ApiException;
+import com.google.maps.model.DistanceMatrix;
 import com.google.maps.model.GeocodingResult;
-public class EarthSearch {
+import com.google.maps.model.TravelMode;
 
-	// Lookups up and returns the address of an establishemnt given its name and possible some locatioin attributes
-	public static void lookupAddress(String establishment) throws ApiException, InterruptedException, IOException {
+/*
+ * Author: @Celethor (Benjamin Treesh)
+ * 
+ * THIs Class contains methods to lookup the address of an establishment given its name, 
+ * It also contains a method to determine the distance between 2 establishments
+ * 
+ * !!NOTE!! YOU WILL NEED TO ACTIVATE BOTH THE GOOGLE GEOCODING API AND THE GOOGLE DISTANCE MATRIX APIS
+ *  FOR YOUR PROJECT IN ORDER TO USE THIS CLASS!!! THANK YOU!! :D
+ * 
+ * credit to google-maps-services-java project on github for help in this method
+ * https://github.com/googlemaps/google-maps-services-java
+ */
+
+
+
+public class EarthSearch {
+	
+	private static final String API_KEY = "AIzaSyD2MvqQVbfXo3M0mMu4JPGXbaN3y5z9SIg";
+
+	// Lookups up and returns the address of an establishment given its name and possible some location attributes
+	public static String lookupAddress(String establishment) throws ApiException, InterruptedException, IOException {
+		
+		//set up key
 		GeoApiContext lookupDoodad = new GeoApiContext.Builder()
-			    .apiKey("AIzaSyD2MvqQVbfXo3M0mMu4JPGXbaN3y5z9SIg")
+			    .apiKey(API_KEY)
 			    .build();
 			GeocodingResult[] results =  GeocodingApi.geocode(lookupDoodad,
 			    establishment).await();
-			Gson gson = new GsonBuilder().setPrettyPrinting().create();
 			
+			//converts results into usable address
+			
+			String address = (results[0].formattedAddress);
 		
-			System.out.println(results[0].formattedAddress);
+		return address;
+	}
+	
+	//given two addresses, calculates the driving distance
+	public static String computeDriveDistance(String addrOne, String addrTwo) throws ApiException, InterruptedException, IOException{
+				
+		//set up key
+    	GeoApiContext distCalcer = new GeoApiContext.Builder()
+			    .apiKey(API_KEY)
+			    .build();
+    	
+    	DistanceMatrixApiRequest req = DistanceMatrixApi.newRequest(distCalcer); 
+        DistanceMatrix result = req.origins(addrOne)
+                .destinations(addrTwo)
+                .mode(TravelMode.DRIVING)
+                .avoid(RouteRestriction.TOLLS)
+                .language("en-US")
+                .await();
+        
+				String distApart = result.rows[0].elements[0].distance.toString();
 		
-		
-		return;
+		return distApart;
 	}
 }
